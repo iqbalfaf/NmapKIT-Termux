c6c13f1d633ae743e589cf23b7e872e66a6cffec
Moch Iqbal <iqbalfaf127@gmail.com>
2018-09-05 05:34:14 +0700

    NmapKIT

---
+++ NmapKIT.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+##### Script By IqbalFAF #####
+##### Nmap Toolkit ######
+
+
+#### Warna ####
+cyan='\e[0;36m'
+green='\e[0;34m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+BlueF='\e[1;34m'
+
+clear
+echo" _   _                       _   _______ _____"
+echo"| \ | |                     | | / /_   _|_   _|"
+echo"|  \| |_ __ ___   __ _ _ __ | |/ /  | |   | |"
+echo"| . ` | '_ ` _ \ / _` | '_ \|    \  | |   | |"
+echo"| |\  | | | | | | (_| | |_) | |\  \_| |_  | |"
+echo"\_| \_/_| |_| |_|\__,_| .__/\_| \_/\___/  \_/"
+echo"                      | |"
+echo"                      |_|"
