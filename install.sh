e48ffdb559e847dc61b4747e232b4bce6b0cc792
Moch Iqbal <iqbalfaf127@gmail.com>
2018-09-06 00:08:41 +0700

    Installer Script NmapKIT

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+clear
+echo "Installer NmapKIT"
+sleep 1
+clear
+echo "Update Repo"
+apt update 
+sleep 1
+clear
+echo "Update Repo Selesai"
+sleep 1
+clear
+echo "Installing Figlet"
+apt install figlet -y
+sleep 1 
+clear
+echo "Install Figlet Selesai"
+sleep 1
+clear
+echo "Installing ncurses-utils"
+apt install ncurses-utils -y
+sleep 1
+clear
+echo "Installing Nmap"
+sleep 1
+apt install nmap -y
+sleep 1
+clear
+echo "Install Nmap Selesai"
+sleep 1
+clear
+echo "Install Script NmapKIT"
+wget -O nmapkit https://raw.githubusercontent.com/iqbalfaf/NmapKIT-Termux/master/NmapKIT.sh
+mv nmapkit $PREFIX/bin/
+chmod +x $PREFIX/bin/nmapkit
+sleep 1
+clear
+echo "Install Script NmapKIT Selesai"
+sleep 1
+clear
+echo "================== NmapKIT ================== "
+echo "Original Script Dracnmap"
+echo "Recode IqbalFAF"
+echo ""
+echo "Untuk Menjalankan Tools nya cukup ketikan nmapkit"
+echo "Terimakasih ^_^"
+echo "================== NmapKIT ================== "
+
